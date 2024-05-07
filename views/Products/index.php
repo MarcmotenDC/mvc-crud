1a0c774378a6570a4d7b17b9f2890d2f9e44884e
MarcmotenDC <superman868789@gmail.com>
2024-05-07 11:05:22 -0500

    Add files via upload

---
+++ views/Products/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Products</title>
+</head>
+
+<body>
+<nav>
+        <ul>
+            <li><a href="https://mywebtraining.net/webdev/SteveHarvey/mvc-crud/">Home</a></li>
+            <a href="https://mywebtraining.net/webdev/SteveHarvey/mvc-crud/products/new">New Product</a>
+        </ul>
+    </nav>
+    <h1>Products</h1>
+    <?php foreach ($products as $product) : ?>
+        <p>
+            <a href="./products/<?= htmlspecialchars($product['id']) ?>/show">
+                <?= htmlspecialchars($product["name"]) ?>
+            </a>
+        </p>
+    <?php endforeach; ?>
+
+</body>
+</html>
